704433e89c583ffe84d4d883fc32230529e62713
arinming <jyandysys1@naver.com>
2023-10-16 18:24:10 +0900

    [Feat] : 미소 첫 번째 화면 완성

--- miso_starbucks/lib/miso.dart
+++ miso_starbucks/lib/miso.dart
@@ -58,8 +58,85 @@ class MisoFirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Miso 첫 번째 페이지"),
+      backgroundColor: misoPrimaryColor,
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity, // 가로로 화면을 꽉 채운다
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+                  Text(
+                    "대한민국 1등 홈서비스\n미소를 만나보세요!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 38,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print("예약하기 버튼 클릭");
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 16,
+                        horizontal: 24,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(64),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.add, color: misoPrimaryColor),
+                          SizedBox(width: 6),
+                          Text(
+                            "예약하기",
+                            style: TextStyle(
+                              color: misoPrimaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              /// 서비스 상세 정보
+              Positioned(
+                bottom: 32,
+                child: GestureDetector(
+                  onTap: () {
+                    print("서비스 상세 정보 클릭");
+                  },
+                  child: Container(
+                    color: Colors.white.withOpacity(0.3),
+                    padding: EdgeInsets.all(12),
+                    child: Text(
+                      "서비스 상세정보",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
