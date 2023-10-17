c9f5ce6e551fd56b51b3d03189198f33448adef5
arinming <jyandysys1@naver.com>
2023-10-17 15:50:12 +0900

    [feat] : 온보딩 페이지 만들기

--- onboarding/lib/main.dart
+++ onboarding/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+late SharedPreferences prefs;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // shared_preferences 인스턴스 생성
+  prefs = await SharedPreferences.getInstance();
   runApp(MyApp());
 }
 
@@ -9,9 +18,118 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    /// SharedPreferences에서 온보딩 완료 여부 조회
+    /// issOnboarded에 해당하는 값에서 null을 반환하는 경우, false 할당
+    bool isOnboarded = prefs.getBool("isOnboarded") ?? false;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      // 전역적인 테마 변경
+      theme: ThemeData(
+        // 전체적인 폰트 설정
+        textTheme: GoogleFonts.getTextTheme("Jua"),
+      ),
+      home: isOnboarded ? HomePage() : OnboardingPage(),
+    );
+  }
+}
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IntroductionScreen(
+        pages: [
+          // 첫 번째 페이지
+          PageViewModel(
+            title: "빠른 개발",
+            body: "Flutter의 hot reload는 쉽고 UI 빌드를 도와줍니다.",
+            image: Padding(
+              padding: EdgeInsets.all(32),
+              child: Image.network('https://i.ibb.co/2ZQW3Sb/flutter.png'),
+            ),
+            decoration: PageDecoration(
+              titleTextStyle: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              bodyTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          // 두 번째 페이지
+          PageViewModel(
+            title: "표현력 있고 유연한 UI",
+            body: "Flutter에 내장된 아름다운 위젯들로 사용자를 기쁘게 하세요.",
+            image: Image.network('https://i.ibb.co/LRpT3RQ/flutter2.png'),
+            decoration: PageDecoration(
+              titleTextStyle: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              bodyTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ],
+        next: Text(
+          "Next",
+          style: TextStyle(fontWeight: FontWeight.w600),
+        ),
+        done: Text(
+          "Done",
+          style: TextStyle(fontWeight: FontWeight.w600),
+        ),
+        // done 버튼을 눌렀을 때 HomePage로 이동
+        onDone: () {
+          // Done 클릭 시 isOnboarded = true로 저장
+          prefs.setBool("isOnboarded", true);
+
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomePage(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page!"),
+        // shared_preferences 초기화
+        actions: [
+          IconButton(
+            onPressed: () {
+              prefs.clear();
+            },
+            icon: Icon(Icons.delete),
+          ),
+        ],
+        centerTitle: true, // 가운데 정렬
+      ),
+      body: Center(
+        child: Text(
+          "환영합니다!",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
     );
   }
 }
