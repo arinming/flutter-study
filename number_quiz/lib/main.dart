9cfe2fd571350bbb91d6e39106a6f380f50ece31
arinming <jyandysys1@naver.com>
2023-10-18 15:14:09 +0900

    [feat] : 숫자 퀴즈 앱 완성

--- number_quiz/lib/main.dart
+++ number_quiz/lib/main.dart
@@ -55,46 +55,48 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        color: Colors.pinkAccent,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // quiz
-            Expanded(
-              child: Center(
-                child: Text(
-                  quiz,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 28,
-                    color: Colors.white,
+      backgroundColor: Colors.pinkAccent,
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // quiz
+              Expanded(
+                child: Center(
+                  child: Text(
+                    quiz,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 28,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-            // New Quiz 버튼
-            SizedBox(
-              height: 42,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.white),
-                ),
-                onPressed: () {
-                  // New Quiz 클릭시 퀴즈 가져오기
-                  getQuiz();
-                },
-                child: Text(
-                  "New Quiz",
-                  style: TextStyle(
-                    color: Colors.pinkAccent,
-                    fontSize: 24,
+              // New Quiz 버튼
+              SizedBox(
+                height: 42,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                  ),
+                  onPressed: () {
+                    // New Quiz 클릭시 퀴즈 가져오기
+                    getQuiz();
+                  },
+                  child: Text(
+                    "New Quiz",
+                    style: TextStyle(
+                      color: Colors.pinkAccent,
+                      fontSize: 24,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
