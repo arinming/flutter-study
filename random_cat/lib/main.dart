7e2e35353ed278e77f0f9b49e3558698a4ba65a3
arinming <jyandysys1@naver.com>
2023-10-18 16:07:05 +0900

    [랜덤 고양이] : 좋아요 사진 기기에 저장하기

--- random_cat/lib/main.dart
+++ random_cat/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  // main() 함수에서 async 쓰려면 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // shared_preferences 인스턴스 생성
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => CatService()),
+        ChangeNotifierProvider(create: (context) => CatService(prefs)),
       ],
       child: const MyApp(),
     ),
@@ -33,8 +40,15 @@ class CatService extends ChangeNotifier {
   // 좋아요 사진
   List<String> favoriteImages = [];
 
-  CatService() {
+  // SharedPreferences 인스턴스
+  SharedPreferences prefs;
+
+  CatService(this.prefs) {
     getRandomCatImages();
+
+    // favorite로 저장된 favoriteImages를 가져온다
+    // 저장된 값이 없는 경우 null, 즉 빈 배열을 넣는다
+    favoriteImages = prefs.getStringList("favorites") ?? [];
   }
 
   // 랜덤 고양이 사진 API 호출
@@ -58,6 +72,10 @@ class CatService extends ChangeNotifier {
     } else {
       favoriteImages.add(catImage); // 추가
     }
+
+    // favortieImage를 favorite 라는 이름으로 저장하기
+    prefs.setStringList("favorites", favoriteImages);
+
     notifyListeners(); // 새로 고침
   }
 }
@@ -143,6 +161,43 @@ class FavoritePage extends StatelessWidget {
           appBar: AppBar(
             title: Text("좋아요"),
             backgroundColor: Colors.amber,
+          ),
+          body: GridView.count(
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+            padding: EdgeInsets.all(8),
+            crossAxisCount: 2,
+            children: List.generate(
+              catService.favoriteImages.length,
+              (index) {
+                String catImage = catService.favoriteImages[index];
+                return GestureDetector(
+                  onTap: () {
+                    catService.toggleFavoriteImage(catImage);
+                  },
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Image.network(
+                          catImage,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Positioned(
+                        right: 8,
+                        bottom: 8,
+                        child: Icon(
+                          Icons.favorite,
+                          color: catService.favoriteImages.contains(catImage)
+                              ? Colors.amber
+                              : Colors.transparent,
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
           ),
         );
       },
